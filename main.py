23e3fd6a51871e6cfdecc8e2474131f5d36edaa8
Новиков Илья <nov.ilucha@yandex.ru>
2024-02-15 22:36:16 +0300

    Автоформатирование кода

--- main.py
+++ main.py
@@ -6,38 +6,45 @@ import sys
 from config import Settings
 from yc_module import NoInstancesException
 
+
 def check_range_timedelta(value):
     ivalue = int(value)
     if ivalue < 5 or ivalue > 3600:
-        raise ArgumentTypeError(
-            "%s is an invalid positive int value" % value)
+        raise ArgumentTypeError("%s is an invalid positive int value" % value)
     return ivalue
 
 
-parser: ArgumentParser = ArgumentParser(
-    prog='batch_write_utils.py')
+parser: ArgumentParser = ArgumentParser(prog="batch_write_utils.py")
 parser.add_argument(
-    "-t", "--timedelta", type=check_range_timedelta,
-    required=False, default=300,
-    help="Время в секундах между повторными запросами от 5sec до 1h")
+    "-t",
+    "--timedelta",
+    type=check_range_timedelta,
+    required=False,
+    default=300,
+    help="Время в секундах между повторными запросами от 5sec до 1h",
+)
 parser.add_argument(
     "-l",
     "--level",
     type=str,
     choices=["debug", "info"],
     default="info",
-    help="уровень отладочной информации"
+    help="уровень отладочной информации",
 )
+
 
 def main(argv):
     args = parser.parse_args()
     from logger import conf_logger
+
     conf_logger(args.level)
 
     import logging
+
     logger = logging.getLogger(__name__)
 
     from yc_module import up_instances
+
     config: Settings = Settings()
 
     try:
@@ -61,6 +68,6 @@ def main(argv):
 
     return 0
 
-if __name__ == '__main__':
-    sys.exit(main(sys.argv))
 
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
